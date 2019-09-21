6c74f8d84932e110c840dc8b3b95d023b59774f7
azuline <azuline@riseup.net>
2019-09-21 13:24:27 -0400

    cracked mt19937 seeded off timestamp~

---
+++ src/22-crack-a-mt19937-seed.py
@@ -0,0 +1,28 @@
+from time import time, sleep
+from random import randint
+from importlib import import_module
+
+twentyone = import_module('21-implement-the-mt19937-mersenne-twister-rng')
+MersenneTwister = twentyone.MersenneTwister
+
+
+def random_wait():
+    rand_wait = randint(40, 100) // 100  # Not patient enough for this.
+    sleep(rand_wait)
+
+
+def crack_seed(random_bit):
+    for i in range(int(time()), int(time()) - 100, -1):
+        if MersenneTwister(i).extract_number() == random_bit:
+            return i
+    raise Exception("Seed not found.")  # Shouldn't happen.
+
+
+if __name__ == '__main__':
+    random_wait()
+    seed = int(time())
+    mt = MersenneTwister(seed)
+    random_wait()
+    random_bit = mt.extract_number()
+    assert seed == crack_seed(random_bit)
+    print('Passed')
